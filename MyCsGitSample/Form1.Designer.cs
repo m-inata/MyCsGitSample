fe1d9f572458db3be689eca7a791c12dec26c3e0
m-inata <m.inata303@gmail.com>
2025-02-13 11:56:26 +0900

    Create button and label

--- MyCsGitSample/Form1.Designer.cs
+++ MyCsGitSample/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.countupButton = new System.Windows.Forms.Button();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // countupButton
+            // 
+            this.countupButton.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.countupButton.Location = new System.Drawing.Point(156, 259);
+            this.countupButton.Name = "countupButton";
+            this.countupButton.Size = new System.Drawing.Size(460, 122);
+            this.countupButton.TabIndex = 0;
+            this.countupButton.Text = "CountUp!";
+            this.countupButton.UseVisualStyleBackColor = true;
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.countLabel.Location = new System.Drawing.Point(164, 63);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(55, 60);
+            this.countLabel.TabIndex = 1;
+            this.countLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.countupButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button countupButton;
+        private System.Windows.Forms.Label countLabel;
     }
 }
 
